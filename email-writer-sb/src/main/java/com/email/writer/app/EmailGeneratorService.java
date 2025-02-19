6d66b36730b86389f097398410a35ea90112f786
OuaailElaouad <2003waelelaouad@gmail.com>
2025-02-19 14:03:09 +0100

    Craft a request

--- email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/writer/app/EmailGeneratorService.java
@@ -1,15 +1,44 @@
 package com.email.writer.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class EmailGeneratorService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String gemeniApiKey;
+
+    public EmailGeneratorService(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     public String generateEmailReply(EmailRequest emailRequest) {
         //Build the prompt
         String prompt = buildPrompt(emailRequest);
         // Craft a request
+        Map<String, Object> requestBody = Map.of(
+                "content", new Object[]{
+                        Map.of("parts", new Object[]{
+                                Map.of("text", prompt)
+                        })
+                }
+        );
         //Do request and get response
+        String response = webClient.post()
+                .uri(geminiApiUrl + gemeniApiKey)
+                .header("Content-Type", "application/json")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
         //Return response
         return prompt;
     }
